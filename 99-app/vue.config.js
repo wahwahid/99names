ad145d0e3f38847af82273bb79d6e73a2e5c00c0
wahwahid <wahid.dulrohman@gmail.com>
2019-12-08 21:00:14 +0700

    99 APP : PWA

--- 99-app/vue.config.js
+++ 99-app/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: '99 Asma Ul Husna',
+    themeColor: '#1976d2',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    assetsVersion: '1'   
+  }
 }
