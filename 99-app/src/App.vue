7e88c6d5e455d2b34b207e86061f4222e66552b0
wahwahid <wahid.dulrohman@gmail.com>
2019-12-08 21:16:46 +0700

    About

--- 99-app/src/App.vue
+++ 99-app/src/App.vue
@@ -12,6 +12,52 @@
         <v-icon>mdi-home</v-icon>
       </v-btn>
       <v-toolbar-title class="pl-1">99 Asma Ul Husna</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-dialog
+        v-model="about"
+        width="500"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn depressed color="primary" v-on="on">
+            About
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            About
+          </v-card-title>
+          <v-card-text class="py-4">
+            <div class="headline">
+              99 Asma Ul Husna
+            </div>
+            <p class="mb-0">
+              Presented by :
+              <ul>
+                  <li>wahwahid</li>
+                  <li>andrisiswanto</li>
+                  <li>kdzikry</li>
+                  <li>lukmansurya</li>
+                  <li>andrianto69</li>
+              </ul>
+            </p>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="about = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-app-bar>
 
     <v-content>
@@ -24,7 +70,7 @@
 export default {
   name: 'App',
   data: () => ({
-    //
+    about: false
   }),
   methods: {
     goBack() {
