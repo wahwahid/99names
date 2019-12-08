987b1860f3a19a7f4b817cab0fc13612b950fe03
wahwahid <wahid.dulrohman@gmail.com>
2019-12-08 17:23:48 +0700

    99 App Base

---
+++ 99-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <span v-if="$route.name === 'home'" style="width: 36px">
+        <v-icon>mdi-home</v-icon>
+      </span>
+      <v-btn icon v-else @click="goBack">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-toolbar-title class="pl-1">99 Asmaul Husna</v-toolbar-title>
+    </v-app-bar>
+
+    <v-sheet id="content">
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </v-sheet>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
+  }
+};
+</script>
