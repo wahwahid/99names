73c7e0e030eda47c9a820d2f55c6da64f21076c5
wahwahid <wahid.dulrohman@gmail.com>
2019-12-08 11:29:37 +0700

    Add Endpoint for Asmaul Husna

--- 99-api/app.js
+++ 99-api/app.js
@@ -1,10 +1,22 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9999
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/data/about.html'))
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/list', (req, res) => {
+    const data = require('./data/asmaul_husna_simple')
+    res.send(data)
+})
+
+app.get('/detail/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const data = require('./data/asmaul_husna')
+    const detail = data.find(v => v.id === id)
+    res.send(detail)
+})
+
+app.listen(port, () => console.log(`99 API listening on port ${port}!`))
